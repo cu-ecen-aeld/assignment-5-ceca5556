ba0b6a5ac0adf8e3150098f6c81d6f80fd58e9cf
ceca5556 <ceca5556@colorado.edu>
2024-10-17 15:12:53 -0600

    assgn3-5 latest commit hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '25a257c9706e6801b83dd4528ae9a238e5523e5c'
+AESD_ASSIGNMENTS_VERSION = '3199b4e740856a31c0de90b4b6ed66966bc44624'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
